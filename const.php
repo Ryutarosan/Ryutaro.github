3468a5e8dd8de4689d0e5e6928c7bb30bda88f22
Ryutarosan <51022618+Ryutarosan@users.noreply.github.com>
2019-07-23 18:31:19 +0900

    Create const.php

---
+++ const.php
@@ -0,0 +1,14 @@
+<?php
+
+// MySQL接続情報
+$USER   = 'codecamp20909';           // MySQLのユーザ名
+$PASSWD = 'NYIERAXN';           // MySQLのパスワード
+$DBNAME = 'codecamp20909';           // データベース名
+$HOST   = 'localhost';      // データベースのホスト名又はIPアドレス
+$CHARSET = 'utf8';          // データベースの文字コード
+$DSN = 'mysql:dbname='.$DBNAME .';host='.$HOST.';charset='.$CHARSET;  // PDOのDSN
+
+// 画像ファイルのディレクトリ
+$IMG_DIR = './images/';
+
+?>
